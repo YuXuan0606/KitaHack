4cc7be110cd01c62a77ee132d1561bf1f5c52e55
YuXuan0606 <teohyuxuan666@gmail.com>
2025-04-08 02:56:02 +0800

    Registration activities

---
+++ app/src/main/java/com/example/kitahack/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.kitahack;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private TextView welcomeTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+
+        // Get the user's name passed from the SignupActivity
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+
+        // Display the welcome message with the user's name
+        welcomeTextView.setText("Welcome, \n" + userName + "!");
+    }
+}
